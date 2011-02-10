551edd26784be8123436ea58bc38a1c2214a452a
kamiccolo <kamiccolo@kamiccolo-laptop.(none)>
2011-02-10 12:42:47 +0200

    Merging.

---
+++ src/BigNum.pas
@@ -0,0 +1,175 @@
+unit BigNum;
+
+interface
+
+const
+	BIGNUM_DIGITS = 254;
+
+type
+	BigNumType = record
+		data: array [1..BIGNUM_DIGITS] of Byte; // or some arbitrary large number
+		positive: Boolean;
+	end;
+	
+	procedure bignum_init(var num: BigNumType);
+	
+	function bignum_tostring(num: BigNumType): string;
+	function bignum_fromstring(str: string): BigNumType;
+
+	function bignum_subtract(a, b: BigNumType): BigNumType;
+{	
+	function bignum_divide(a, b: BigNumType): BigNumType;
+	function bignum_remainder(a, b: BigNumType): BigNumType;
+}
+	
+	function bignum_add(a, b: BigNumType): BigNumType;
+
+implementation
+
+procedure bignum_init(var num: BigNumType);
+var
+	i: Integer;
+begin
+	for i := 1 to BIGNUM_DIGITS do
+		num.data[i] := 0;
+		num.positive := true;
+end;
+
+function bignum_tostring(num: BigNumType): string;
+var
+	i: Integer;
+	found_num: Boolean;
+	res: string;
+begin
+	found_num := false;
+	res := '';
+	for i := BIGNUM_DIGITS downto 1 do 
+	begin
+		if (not found_num) and (num.data[i] <> 0) then
+			found_num := true;
+		if (found_num) or (i = 1) then
+		begin
+			res := res + chr(ord('0') + num.data[i]);
+		end;
+	end;
+	
+	if found_num and (not num.positive) then
+		res := '-' + res;
+	
+	bignum_tostring := res;
+end;
+
+function bignum_fromstring(str: string): BigNumType;
+var
+	i: Integer;
+	num: BigNumType;
+begin
+	bignum_init(num);
+	if length(str) <= BIGNUM_DIGITS then
+		for i := length(str) downto 1 do
+		begin
+			if (str[i] >= '0') and (str[i] <= '9') then
+				num.data[length(str) - i + 1] := ord(str[i]) - ord('0')
+			else if (str[i] = '-') then
+				num.positive := false
+			else
+				num.data[length(str) - i + 1] := 0;
+		end;
+	
+	bignum_fromstring := num;
+end;
+
+function bignum_add(a, b: BigNumType): BigNumType;
+var
+	cf, af: Boolean;
+	i: integer;
+	sum: BigNumType;
+	buf: byte;
+	tmp: BigNumType;
+begin
+	bignum_init(sum);
+	cf:=false;  //not used
+	af:=false;
+	buf:=0;
+	
+	if (a.positive xor b.positive) then
+	begin  //adding numbers with different signs
+		tmp:=a;
+		if(a.positive=false) then  //let a be positive number and b - negative
+		begin
+			a:=b;
+			b:=tmp;
+		end;
+		
+		for i:=1 to BIGNUM_DIGITS do
+			begin
+			if (i<>1) then
+			begin
+				if (af) then
+				begin
+					if (a.data[i]=0) then  //borrowing from zero
+					begin
+						af:=true;
+						a.data[i]:=9;
+					end
+					else
+					begin
+						a.data[i]:=a.data[i]-1;  //still no need to borrow
+						af:=false;
+					end;
+				end;
+			end;
+			
+			if (a.data[i]<b.data[i]) then
+			begin
+				sum.data[i]:=a.data[i]+10-b.data[i];
+				af:=true;
+			end
+			else
+			begin
+				sum.data[i]:=a.data[i]-b.data[i];
+				//af:=false;
+			end;
+		end;
+		sum.positive:=true;
+		
+		
+		if (af) then  //negative number!
+		begin
+			a:=b;
+			b:=tmp;
+			a.positive:=true;
+			b.positive:=false;
+			
+			sum:=bignum_add(a, b);
+			sum.positive:=false;
+		end;	        
+	end
+	else
+	begin  //adding numbers with same signs
+		for i:=1 to BIGNUM_DIGITS do
+		begin
+			buf:=a.data[i]+b.data[i];
+			if (af=true) then
+			begin
+				buf:=buf+1;
+				af:=false;
+			end;
+			if (buf>9) then af:=true;
+			sum.data[i]:=buf mod 10;
+		end;
+		sum.positive:=a.positive;
+	end;
+	
+	bignum_add:=sum;
+end;
+
+function bignum_subtract(a, b: BigNumType): BigNumType;
+begin
+	b.positive := not b.positive;
+	bignum_subtract := bignum_add(a, b);
+end;
+
+initialization
+
+end.
---
+++ test/BigNumTest.pas
@@ -0,0 +1,129 @@
+unit BigNumTest;
+{$mode objfpc}
+
+interface
+uses
+	fpcunit, testregistry, BigNum;
+
+type
+	BigNumTestCase = class(TTestCase)
+	Published
+		Procedure InitTest;
+		Procedure ToStringTest;
+		Procedure FromStringTest;
+		Procedure AdditionTest;
+		Procedure SubtractionTest;
+	end;
+	
+implementation
+
+Procedure BigNumTestCase.InitTest;
+var
+	num: BigNumType;
+begin
+	bignum_init(num);
+	AssertEquals('BigNum should be initialized to zero.',bignum_tostring(num),'0');
+	AssertEquals('BigNumType.positive should be initialized to 1.',num.positive,true);
+	
+end;
+
+Procedure BigNumTestCase.ToStringTest;
+var
+	num: BigNumType;
+begin
+	bignum_init(num);
+	num.data[1] := 2;
+	num.data[2] := 4;
+	AssertEquals('Simple 42 test.', '42', bignum_tostring(num));
+	
+	bignum_init(num);
+	num.data[1] := 0;
+	num.data[2] := 2;
+	num.data[3] := 4;
+	AssertEquals('Number ending with a zero.', '420', bignum_tostring(num));
+	
+	bignum_init(num);
+	num.data[1] := 1;
+	num.data[2] := 0;
+	num.data[3] := 2;
+	num.data[4] := 4;
+	AssertEquals('Number with a zero digit in the middle.', '4201', bignum_tostring(num));
+	
+	bignum_init(num);
+	num.positive := False;
+	num.data[1] := 1;
+	AssertEquals('minus one', '-1', bignum_tostring(num));
+	
+	bignum_init(num);
+	num.positive := False;
+	num.data[1] := 2;
+	num.data[2] := 4;
+	AssertEquals('mainus fourty two', '-42', bignum_tostring(num));
+	// TODO proper tests for limits
+end;
+
+Procedure BigNumTestCase.FromStringTest;
+var
+	num: BigNumType;
+begin
+	num := bignum_fromstring('1');
+	
+	AssertEquals('1!', '1', bignum_tostring(bignum_fromstring('1')));
+	AssertEquals('0!', '0', bignum_tostring(bignum_fromstring('0')));
+	AssertEquals('passing empty string should yield zero', '0', bignum_tostring(bignum_fromstring('')));
+	num := bignum_fromstring('1232112423');
+	AssertEquals('Medium length string test', '1232112423', bignum_tostring(num));
+	num := bignum_fromstring('0000');
+	AssertEquals('Bunch of zeroes test.', '0', bignum_tostring(num));
+	num := bignum_fromstring('2132132001223232132111123213213211');
+	AssertEquals('Number with a zero in the middle', '2132132001223232132111123213213211', bignum_tostring(num));
+	num:=bignum_fromstring('-5');
+	AssertEquals('Tostring-fromstring with negative numbers.','-5',bignum_tostring(num));
+	AssertEquals('negative large number', bignum_tostring(bignum_fromstring('-2134343578439758437589743957439578437598437574385743957843')), '-2134343578439758437589743957439578437598437574385743957843');
+	// TODO proper tests for limits
+end;
+
+Procedure BigNumTestCase.AdditionTest;
+var
+	num, num2: BigNumType;
+	i: integer;
+begin
+	num := bignum_fromstring('5');
+	num2:=bignum_add(num, num);
+	AssertEquals('BigNum_add. Adding two positive integers. Sum below 999....99.','10', bignum_tostring(num2));
+	AssertEquals('0+0 == 0', '0', bignum_tostring(bignum_add(bignum_fromstring('0'), bignum_fromstring('0'))));
+	AssertEquals('1+0 == 1.', '1', bignum_tostring(bignum_add(bignum_fromstring('1'), bignum_fromstring('0'))));
+	AssertEquals('1+0 == 1.', '1', bignum_tostring(bignum_add(bignum_fromstring('1'), bignum_fromstring('0'))));
+	for i:=1 to BIGNUM_DIGITS do num.data[i]:=9;
+	num2:=bignum_fromstring('1');
+	AssertEquals('Adding biggest possible number with 1, (causing an overflow).','0',bignum_tostring(bignum_add(num, num2)));
+	AssertEquals('Adding biggest possible number with 1, (causing an overflow). Part 2.','0',bignum_tostring(bignum_add(bignum_fromstring(bignum_tostring(num)), num2)));
+	num:=bignum_fromstring('-5');
+	num2:=bignum_fromstring('8');
+	AssertEquals('Adding positive and negative numbers.','3',bignum_tostring(bignum_add(num2, num)));
+	
+	bignum_init(num);
+	num.data[1]:=5;
+	num.positive:=false;
+	num2:=bignum_fromstring('3');
+	AssertEquals('Adding positive and negative numbers. Part 2.','-2',bignum_tostring(bignum_add(num2, num)));
+	num2:=bignum_fromstring('8');
+	AssertEquals('Adding positive and negative numbers. Part 3.','-3',bignum_tostring(bignum_add(num2, num)));
+	
+	AssertEquals('<Hugenum> + <Hugenum>.', '109999999999999999999999999999999999999999990', bignum_tostring(bignum_add(bignum_fromstring('9999999999999999999999999999999999999999999'), bignum_fromstring('99999999999999999999999999999999999999999991'))));
+end;
+
+Procedure BigNumTestCase.SubtractionTest;
+var
+	num: BigNumType;
+begin
+	AssertEquals('1 - 1 == 0', '0', bignum_tostring(bignum_subtract(bignum_fromstring('1'), bignum_fromstring('1'))));
+	AssertEquals('(-1) - (-1) == 0', '0', bignum_tostring(bignum_subtract(bignum_fromstring('-1'), bignum_fromstring('-1'))));
+	AssertEquals('2 - 1 == 1', '1', bignum_tostring(bignum_subtract(bignum_fromstring('2'), bignum_fromstring('1'))));
+	AssertEquals('digit carry test: 10 - 1 == 9', '9', bignum_tostring(bignum_subtract(bignum_fromstring('10'), bignum_fromstring('1'))));
+	AssertEquals('global carry 2 - 3 == -1', '-1', bignum_tostring(bignum_subtract(bignum_fromstring('2'), bignum_fromstring('3'))));
+end;
+
+initialization
+	RegisterTest(BigNumTestCase);
+end.
